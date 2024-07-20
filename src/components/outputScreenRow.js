1f403d97fbce15c47d37ba7de7db9ffe40f6bd20
Frank <frankkashere@gmail.com>
2024-07-20 20:13:36 +0300

    Second commit

---
+++ src/components/outputScreenRow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OutputScreenRow = () => {
+    return (
+        <div className = "screen-row">
+            <input type="text" readOnly={true}/>
+        </div>
+    );
+};
+
+export default OutputScreenRow;
